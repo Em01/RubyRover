acaddf7b4ad87ba59e456b4a8907907bc4cf4b3e
Emma <emma_williams1@hotmail.co.uk>
2014-07-15 12:08:57 +0100

    initial rover tests passing

--- lib/rover.rb
+++ lib/rover.rb
@@ -6,7 +6,11 @@ def initialize
   end
 
   def direction(current_position)
-    current_position.split[2]
+    current_position.split.last
+  end
+
+  def coordinates(current_coordinates)
+    current_coordinates.split.take(2).join(',')
   end
 
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -3,15 +3,17 @@ require 'rover'
 
 
   describe Rover do
-
-
   let (:rover) {Rover.new}
 
   
   context 'Rover position'
 
     it 'should know its start facing direction' do 
-    expect(rover.direction("0 0 N")).to eq "N"
+    expect(rover.direction("1 2 N")).to eq ("N")
+  end
+
+    it 'should know its start coordinates' do 
+      expect(rover.coordinates("1 2 N")).to eq ("1,2")
   end
 
 end
